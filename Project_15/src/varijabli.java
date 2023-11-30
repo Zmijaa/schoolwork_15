a29736365710e4a6ce3509c8cefdeef3c9f599b2
Filip Mircev <147530688+Zmijaa@users.noreply.github.com>
2023-11-30 09:17:52 +0100

    Add files via upload

---
+++ Project_15/src/varijabli.java
@@ -0,0 +1,24 @@
+
+public class varijabli {
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		int x,y,z;
+if(x>y||z>y)
+System.out.println("1");
+else
+	System.out.println("2");
+if(Math.abs(x-y)>-z)
+	if(x>y)
+		System.out.println("4");
+	else
+		System.out.println("5");
+
+	}
+
+}
+//Што ќе се покаже ако
+//i. x=1, y=1, z=1
+//ii. x=2, y=1, z=o
+//iii. x=3, y=5, z=4
+//За решавање на задачата променувај по овие формули
